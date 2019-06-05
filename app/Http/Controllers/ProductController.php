bda4305864696e79be22143c153a7ad390d2145f
khanhtungmtp <umlatao97@gmail.com>
2019-06-05 12:09:53 +0700

    code productController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,6 +15,7 @@ class ProductController extends Controller
     public function index()
     {
         //
+        return response()->json(Product::all(), 200);
     }
 
     /**
@@ -25,34 +26,58 @@ class ProductController extends Controller
     public function create()
     {
         //
+
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         //
+        $product = Product::create([
+            'name'        => $request->name,
+            'description' => $request->description,
+            'units'       => $request->units,
+            'price'       => $request->price,
+            'image'       => $request->image,
+        ]);
+
+        return response()->json([
+            'status'  => (bool)$product,
+            'data'    => $product,
+            'message' => $product ? 'Thêm sản phẩm thành công' : 'Thêm sản phẩm thất bại'
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Product  $product
+     * @param \App\Product $product
      * @return \Illuminate\Http\Response
      */
     public function show(Product $product)
     {
         //
+        return response()->json($product, 200);
+    }
+
+    public function uploadFile(Request $request)
+    {
+        if ($request->hasFile('image')){
+            $name = time() . '_' . $request->file('image')->getClientOriginalName();
+            $request->file('image')->move(public_path('images'), $name);
+        }
+        return response()->json(asset("images/$name"), 201);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Product  $product
+     * @param \App\Product $product
      * @return \Illuminate\Http\Response
      */
     public function edit(Product $product)
@@ -63,23 +88,47 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Product  $product
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Product             $product
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Product $product)
     {
         //
+        $product = $product->update(
+            $request->only('name', 'description', 'units', 'price', 'image')
+        );
+
+        return response()->json([
+            'data' => $product,
+            'message' =>$product ? 'Cập nhập sản phẩm thành công' : 'Cập nhập sản phẩm thất bại'
+        ]);
+    }
+
+    public function updateUnits(Request $request, Product $product)
+    {
+        $product->units = $product->units + $request->get('units');
+        $product = $product->save();
+
+        return response()->json([
+            'data' =>$product,
+            'message' => $product ? 'Thêm đơn vị tiền thành công' : 'Thêm đơn vị tiền thất bại'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Product  $product
+     * @param \App\Product $product
      * @return \Illuminate\Http\Response
      */
     public function destroy(Product $product)
     {
         //
+        $product = $product->delete();
+        return response()->json([
+            'data' => $product,
+            'message' => $product ? 'Xóa sản phẩm thành công' : 'Xóa sản phẩm thất bại'
+        ]);
     }
 }
